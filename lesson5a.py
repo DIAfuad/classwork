2519523b356d6423bd634bc78134a651d2db9920
DIAfuad <56949264+DIAfuad@users.noreply.github.com>
2019-10-24 11:40:05 +0600

    Add files via upload

--- lesson5a.py
+++ lesson5a.py
@@ -1,2 +1,17 @@
 #python loop
 #==============
+
+"""
+Syntax of for loop
+
+for variable in sequence:
+    Body of for
+"""
+
+for x in [7,33,5,22]:
+    print(x,end="\t")
+
+for x in range(0,99):
+    print(x,end="\t")
+    print(x)
+    print(x,end=" ")
