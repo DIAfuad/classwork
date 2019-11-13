d33bce0cf81d01e8a4f9618a047492a8cb2cc038
DIAfuad <56949264+DIAfuad@users.noreply.github.com>
2019-11-14 02:42:44 +0600

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,29 @@
+Fuad={'Name':'Fuad Mahbub','ID':1001076,'Email':'fuadmahbub16@gmail.com'}
+Alvy={'Name':'Sadekin Alvy','ID':1001074,'Email':'sadekinalvy@gmail.com'}
+Abir={'Name':'Abir Rahman','ID':1001070,'Email':'abir@gmail.com'}
+Shafin={'Name':'Shafin Mohammad','ID':1001053,'Email':'shafin@gmail.com'}
+Munna={'Name':'Munna Ahmed','ID':1001056,'Email':'munna32@gmail.com'}
+
+print("Keys and Values")
+for key in Fuad.keys():
+    print(key + " - " + str(Fuad[key]))
+
+for key in Alvy.keys():
+    print(key + " - " + str(Alvy[key]))
+
+for key in Abir.keys():
+    print(key + " - " + str(Abir[key]))
+
+for key in Shafin.keys():
+    print(key + " - " + str(Shafin[key]))
+
+for key in Munna.keys():
+    print(key + " - " + str(Munna[key]))
+
+
+
+
+
+
+
+
