8ec1b6d0f99ddadb04f3ee0170a014e9afaeaeb4
DIAfuad <56949264+DIAfuad@users.noreply.github.com>
2019-11-24 12:21:25 +0600

    Add files via upload

---
+++ Lesson10 Method.py
@@ -0,0 +1,53 @@
+#functions=block of codes
+#OOP=Object Oriented Programs
+
+def Display():
+    print('This is line 1')
+    print('This is line 2')
+    print('This is line 3')
+    print('This is line 4')
+    print('This is line 5')
+    print('Prepared by XYZ')
+
+def Show(x):
+    print('My name is %s' % (x))
+    print('%s is a bad boy' % (x))
+    print('%s eka na' % (x))
+
+Display()
+print()
+Display()
+print()
+Display()   
+
+Show('Fuad')
+print()
+Show('Fuad')
+print()
+Show('Fuad')
+print()
+Show('Shaon')
+
+def friends(x,y):
+    print('%s and %s are just good friends' % (x,y))
+    
+friends('Fuad','Shaon')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
